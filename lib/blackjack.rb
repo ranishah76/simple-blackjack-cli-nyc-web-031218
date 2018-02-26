3555ef5b95ad71be2eccb7ffd9b0b96a98f4791c
Rani Shah <rani76@gmail.com>
2018-02-26 20:05:22 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,9 +52,11 @@ end
 #####################################################
 
 def runner
-  welcome 
-  initial_round
-  until card_total > 21 
-    hit?
+  welcome
+  card_ total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
 end
+  end_game(card_total)
 end
